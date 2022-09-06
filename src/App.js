7307a73abc3e2e4795d6b3e59ba6c65ba9b0d403
VBZ <80421293+buzzracharyeah-rejan@users.noreply.github.com>
2022-09-06 21:00:30 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import SelectComponent from "./SelectComponent";
+import "./styles.css";
+
+const options = [
+  { key: 1, value: "Test 1" },
+  { key: 2, value: "Test 2" },
+  { key: 3, value: "Test 3" },
+  { key: 4, value: "Test 4" }
+];
+export default function App() {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  return (
+    <div className="App">
+      <h1>Custom Dropdown</h1>
+      <SelectComponent
+        options={options}
+        onChange={(item) => setSelectedOption(item)}
+        selectedKey={selectedOption}
+        placeholder={"type to search"}
+      />
+      <p>selected option: {selectedOption}</p>
+    </div>
+  );
+}
